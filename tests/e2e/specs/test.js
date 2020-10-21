9f642a58c63cd56e999e93e3e757fcdb01fa3f19
Alan Parley <alanparley@hotmail.com>
2020-10-21 19:52:57 +0100

    unit tests running aok

---
+++ tests/e2e/specs/test.js
@@ -0,0 +1,129 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('update the display of the running total', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '4')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should update the display with the result of the operation', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '10')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should perform multiple operations chained together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '12')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should handle positive numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '18')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should handle negative numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-8')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should handle numbers with decimals', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '4.5')
+  })
+})
+
+describe('calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('should handle large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '43046721000')
+  })
+})
---
+++ tests/unit/calculator.spec.js
@@ -0,0 +1,83 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  it('enterNum changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('5');
+    expect(wrapper.vm.runningTotal).to.equal(9)
+  })
+})
+
+describe('App.vue', () => {
+  it('adds two numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('1');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  })
+})
+
+describe('App.vue', () => {
+  it('subtracts two numbers ', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+})
+describe('App.vue', () => {
+  it('multiplies two numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  })
+})
+describe('App.vue', () => {
+  it('divides two numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+})
+describe('App.vue', () => {
+  it('concatenates multiple number button clicks', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 0
+    wrapper.vm.numberClick('4');
+    wrapper.vm.numberClick('3');
+    wrapper.vm.numberClick('4');
+    expect(wrapper.vm.runningTotal).to.equal(434)
+  })
+})
+describe('App.vue', () => {
+  it('chains multiple operations together', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 0
+    wrapper.vm.numberClick('4');
+    wrapper.vm.operatorClick('*');
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('+');
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('-');
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('/');
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(4)
+  })
+})
+describe('App.vue', () => {
+  it('clears the running total without affecting the calculation', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    wrapper.vm.operatorClick('=');
+    wrapper.vm.clearClick();
+    expect(wrapper.vm.runningTotal).to.equal(0)
+  })
+})
